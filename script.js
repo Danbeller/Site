3dd4281ffa80a464aaf3a17f310401386d429495
Danbeller <contatodanbeller@gmail.com>
2026-02-26 13:59:23 -0300

    Ajuste Mobile Menu

--- script.js
+++ script.js
@@ -77,6 +77,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+
+function toggleMobileMenu() {
+    const menu = document.getElementById("mobileMenu");
+
+    if (menu.classList.contains("hidden")) {
+        menu.classList.remove("hidden");
+    } else {
+        menu.classList.add("hidden");
+    }
+}
+
 /* =========================
    SCROLL TO TOP
 ========================== */
